5d71f4018aad536517d40e401602ae88ea571c39
Ece <32641501+eceokumuus@users.noreply.github.com>
2024-06-11 23:57:36 +0300

    contact kısmını mailto ile değiştirdim.

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
       <Projects />
       <Skills />
       <Experience />
-      <Contact />
+      {/* <Contact /> */}
     </main>
   );
 }
--- components/intro.tsx
+++ components/intro.tsx
@@ -4,8 +4,8 @@ import Image from "next/image";
 import React, { useState, useEffect } from 'react';
 import { motion } from "framer-motion";
 import Link from "next/link";
-import { BsArrowRight, BsLinkedin } from "react-icons/bs";
-import { HiDownload } from "react-icons/hi";
+import { BsArrowRight, BsSendSlash , BsSendPlus, BsFillSendCheckFill, BsLinkedin, BsSend, BsSendCheck, BsSendCheckFill, BsSendX } from "react-icons/bs";
+import { HiDownload , HiOutlineMailOpen, HiOutlineMail} from "react-icons/hi";
 import { FaGithub } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 import { useSectionInView } from "@/lib/hooks";
@@ -22,7 +22,7 @@ const useTypingAnimation = (fullText: string, speed: number) => {
       if (index === fullText.length) {
         clearInterval(interval);
       }
-    }, speed); 
+    }, speed);
     return () => clearInterval(interval);
   }, [fullText, speed]);
 
@@ -108,17 +108,17 @@ export default function Intro() {
           delay: 0.1,
         }}
       >
-        <Link
+        {/* <Link
           href="#contact"
           className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
           onClick={() => {
-            setActiveSection("Contact");
+            //setActiveSection("Contact");
             setTimeOfLastClick(Date.now());
           }}
         >
           Contact me here{" "}
           <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
-        </Link>
+        </Link> */}
 
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
@@ -127,6 +127,14 @@ export default function Intro() {
         >
           Download CV{" "}
           <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+        </a>
+
+        <a
+          className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+          href="mailto:eceokumuus@hotmail.com"
+          target="_blank"
+        >
+          <HiOutlineMail />
         </a>
 
         <a
