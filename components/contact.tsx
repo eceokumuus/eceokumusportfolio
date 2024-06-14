dfe58e5a83d061dbfa4a405206f4e0d25b3af61b
Ece <32641501+eceokumuus@users.noreply.github.com>
2024-06-14 14:54:59 +0300

    Disable contact

--- components/contact.tsx
+++ components/contact.tsx
@@ -9,66 +9,66 @@ import SubmitBtn from "./submit-btn";
 import toast from "react-hot-toast";
 
 export default function Contact() {
-  const { ref } = useSectionInView("Contact");
+  // const { ref } = useSectionInView("Contact");
 
-  return (
-    <motion.section
-      id="contact"
-      ref={ref}
-      className="mb-20 sm:mb-28 w-[min(100%,38rem)] text-center"
-      initial={{
-        opacity: 0,
-      }}
-      whileInView={{
-        opacity: 1,
-      }}
-      transition={{
-        duration: 1,
-      }}
-      viewport={{
-        once: true,
-      }}
-    >
-      <SectionHeading>Contact me</SectionHeading>
+  // return (
+  //   <motion.section
+  //     id="contact"
+  //     ref={ref}
+  //     className="mb-20 sm:mb-28 w-[min(100%,38rem)] text-center"
+  //     initial={{
+  //       opacity: 0,
+  //     }}
+  //     whileInView={{
+  //       opacity: 1,
+  //     }}
+  //     transition={{
+  //       duration: 1,
+  //     }}
+  //     viewport={{
+  //       once: true,
+  //     }}
+  //   >
+  //     <SectionHeading>Contact me</SectionHeading>
 
-      <p className="text-gray-700 -mt-6 dark:text-white/80">
-        Please contact me directly at{" "}
-        <a className="underline" href="mailto:eceokumuus">
-          eceokumuus@hotmail.com
-        </a>{" "}
-        or through this form.
-      </p>
+  //     <p className="text-gray-700 -mt-6 dark:text-white/80">
+  //       Please contact me directly at{" "}
+  //       <a className="underline" href="mailto:eceokumuus">
+  //         eceokumuus@hotmail.com
+  //       </a>{" "}
+  //       or through this form.
+  //     </p>
 
-      <form
-        className="mt-10 flex flex-col dark:text-black"
-        action={async (formData) => {
-          const { data, error } = await sendEmail(formData);
+  //     <form
+  //       className="mt-10 flex flex-col dark:text-black"
+  //       action={async (formData) => {
+  //         const { data, error } = await sendEmail(formData);
 
-          if (error) {
-            toast.error(error);
-            return;
-          }
+  //         if (error) {
+  //           toast.error(error);
+  //           return;
+  //         }
 
-          toast.success("Email sent successfully!");
-        }}
-      >
-        <input
-          className="h-14 px-4 rounded-lg borderBlack dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
-          name="senderEmail"
-          type="email"
-          required
-          maxLength={500}
-          placeholder="Your email"
-        />
-        <textarea
-          className="h-52 my-3 rounded-lg borderBlack p-4 dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
-          name="message"
-          placeholder="Your message"
-          required
-          maxLength={5000}
-        />
-        <SubmitBtn />
-      </form>
-    </motion.section>
-  );
+  //         toast.success("Email sent successfully!");
+  //       }}
+  //     >
+  //       <input
+  //         className="h-14 px-4 rounded-lg borderBlack dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
+  //         name="senderEmail"
+  //         type="email"
+  //         required
+  //         maxLength={500}
+  //         placeholder="Your email"
+  //       />
+  //       <textarea
+  //         className="h-52 my-3 rounded-lg borderBlack p-4 dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
+  //         name="message"
+  //         placeholder="Your message"
+  //         required
+  //         maxLength={5000}
+  //       />
+  //       <SubmitBtn />
+  //     </form>
+  //   </motion.section>
+  // );
 }
