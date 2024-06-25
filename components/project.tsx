a23ca1abc6d3bb357246bc0293850f3aa1869c12
Ece <32641501+eceokumuus@users.noreply.github.com>
2024-06-25 17:32:43 +0300

    proje detay modalı kapandı

--- components/project.tsx
+++ components/project.tsx
@@ -4,7 +4,8 @@ import { useState, useRef } from "react";
 import { projectsData } from "@/lib/data";
 import Image from "next/image";
 import { motion, useScroll, useTransform } from "framer-motion";
-import { Modal } from "./modal"; // Modal bileşenini içe aktar
+//import { Modal } from "./modal"; 
+// Modal bileşenini içe aktar
 
 type ProjectProps = (typeof projectsData)[number];
 
@@ -16,7 +17,7 @@ export default function Project({
   linkImage,
   link,
 }: ProjectProps) {
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  //const [isModalOpen, setIsModalOpen] = useState(false);
   const ref = useRef<HTMLDivElement>(null);
   const { scrollYProgress } = useScroll({
     target: ref,
@@ -25,13 +26,13 @@ export default function Project({
   const scaleProgess = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
   const opacityProgess = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
 
-  const openModal = () => {
-    setIsModalOpen(true);
-  };
+  // const openModal = () => {
+  //   setIsModalOpen(true);
+  // };
 
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
+  // const closeModal = () => {
+  //   setIsModalOpen(false);
+  // };
 
   const handleImageClick = (url: string, event: React.MouseEvent) => {
     event.stopPropagation(); // Tıklama olayının yukarıya yayılmasını durdur
@@ -47,7 +48,8 @@ export default function Project({
           opacity: opacityProgess,
         }}
         className="group mb-3 sm:mb-8 last:mb-0 cursor-pointer"
-        onClick={openModal} // Projeye tıklayınca modalı aç
+        //onClick={openModal} 
+        // Projeye tıklayınca modalı aç
       >
         <section className="bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
           <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
@@ -100,14 +102,14 @@ export default function Project({
         </section>
       </motion.div>
 
-      <Modal
+      {/* <Modal
         isOpen={isModalOpen}
         onClose={closeModal}
         title={title}
         description={description}
         tags={tags}
         imageUrl={imageUrl}
-      />
+      /> */}
     </>
   );
 }
